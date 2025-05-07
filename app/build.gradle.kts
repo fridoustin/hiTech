26184858c71080922a0f1ab92b2705cf3e8b3807
fridoustin <fridolinaustin16@gmail.com>
2025-05-07 14:38:10 +0800

    fix: fix build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.hitech"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.hitech"
-        minSdk = 33
-        targetSdk = 34
+        minSdk = 31
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
